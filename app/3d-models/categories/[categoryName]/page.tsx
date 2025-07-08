dab115f571b2f3858f542d127986fe3042831b47
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-07-08 23:32:24 +0300

    feat: update CategoryPage to retrieve and display models for the selected category

--- app/3d-models/categories/[categoryName]/page.tsx
+++ app/3d-models/categories/[categoryName]/page.tsx
@@ -1,9 +1,12 @@
+import ModelsGrid from "@/app/components/ModelsGrid"
 import { getCategoryBySlug } from "@/app/lib/categories"
+import { getModels } from "@/app/lib/models"
 import { CategoryPageProps } from "@/app/types"
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
     const { categoryName } = await params
     const category = getCategoryBySlug(categoryName)
-    console.log(category)
-    return <h1>{category.displayName}</h1>
+    const models = await getModels({ category: categoryName })
+
+    return <ModelsGrid title={category.displayName} models={models} />
 }
