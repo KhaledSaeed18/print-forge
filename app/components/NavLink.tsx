8509a96b9ecd2164a3ef4ce41b8465e13ca808db
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-07-09 23:52:12 +0300

    fix: update NavLink component to use correct hover and active text colors

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -6,8 +6,8 @@ export default function NavLink({ href, children, isActive }: NavLinkProps) {
         <li className="text-sm uppercase">
             <Link
                 href={href}
-                className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-orange-accent 
-                    ${isActive ? "text-orange-accent" : "text-gray-700"}`
+                className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-orange-500 
+                    ${isActive ? "text-orange-500" : "text-gray-700"}`
                 }
             >
                 {children}
