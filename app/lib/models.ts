094eeeb20c179cd76361b1eb2f25e52ac4735762
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-07-09 23:52:54 +0300

    refactor: remove unnecessary comments from getModels and getModelById functions

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -2,9 +2,6 @@ import modelsData from "../data/models.json"
 import type { Model, GetModelsParams } from "../types"
 
 export async function getModels({ category }: GetModelsParams = {}): Promise<Model[]> {
-    // This is where you'd write code to fetch the list
-    // of models from a database. We're mocking that with
-    // our JSON array of data in models.json for now.
     let filteredModels = [...modelsData]
     if (category) {
         filteredModels = modelsData.filter(
@@ -15,9 +12,6 @@ export async function getModels({ category }: GetModelsParams = {}): Promise<Mod
 }
 
 export async function getModelById(id: string | number): Promise<Model> {
-    // These functions don't technically need to be async functions,
-    // but we're planning for the future when they'll be fetching
-    // from a real data source.
     const foundModel = modelsData.find(
         (model: Model) => model.id.toString() === id.toString()
     )
